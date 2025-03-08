815f1da6e8e1df0e3f537503aa8491105deef281
Morteza Gharedaghi <M.Gharedaghi@sadad.co.ir>
2025-03-08 18:32:26 +0330

    Initial Commit

---
+++ NX-Content/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NX-Content
+//
+//  Created by Morteza on 3/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NX-Content/NX_ContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NX_ContentApp.swift
+//  NX-Content
+//
+//  Created by Morteza on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NX_ContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
